b81211d80234b4dcdab43af1ba9e8bada43340a8
Martin Besozzi <embesozzi@gmail.com>
2020-07-18 18:05:33 -0300

    First commit

---
+++ src/main/java/com/mbesozzi/example/demo/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.mbesozzi.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController
+{
+    @GetMapping(value = {"/",""})
+	public String index(){
+		return "Hello";
+	}
+}
+
+
