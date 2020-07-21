34e4381d06cae83cd4d7f869451a0732225d258c
Martin Besozzi <embesozzi@gmail.com>
2020-07-21 15:56:30 -0300

    feat(release): Update re-package after increment version

--- prepare-release.sh
+++ prepare-release.sh
@@ -4,6 +4,9 @@ echo "version = $1"
 # Added version
 # mvn versions:set -DnewVersion=$1
 JAR_VERSION=`echo $1 | cut -d'v' -f2`
+echo "jar = $JAR_VERSION"
 mvn versions:set -DnewVersion=$JAR_VERSION
+
+mvn clean package
 
 printenv
