44232a0b088904dce56f01969c975d9da7e90771
Martin Besozzi <embesozzi@gmail.com>
2020-07-21 16:34:58 -0300

    fix(release): Adjust workflow and release configuration

--- prepare-release.sh
+++ prepare-release.sh
@@ -7,7 +7,7 @@ JAR_VERSION=`echo $1 | cut -d'v' -f2`
 echo "jar = $JAR_VERSION"
 mvn versions:set -DnewVersion=$JAR_VERSION
 
-mvn clean package
-cp target/*.jar release 
+mvn clean package 
+mkdir release && cp target/*.jar release
 
 printenv
